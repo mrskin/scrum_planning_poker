c7215aa6cfd36a79a4fd8d073296c6f0c130e889
spodlecki <spodlecki@skintertainment.com>
2017-05-17 09:57:48 -0500

    initial commit with some basic stuff going on

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    app = express(),
+    config = require('./config');
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+});
+
+app.listen(config.port);
+console.log('listening on port ' + config.port);
